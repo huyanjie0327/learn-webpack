16e03f3255045f7d77faf10b7e1d823a74665b53
huyanjie <huyanjie@touguyun.cn>
2017-04-06 17:04:13 +0800

    添加更多loaders

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import $ from 'jquery';
-$('body').html('Hello');
+import Button from './Components/Button';
+
+const button = new Button('google.com');
+button.render('a');
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 module.exports = {
     entry: './src/',
     output: {
-        path: __dirname + './builds',
+        path: __dirname + '/builds',
         filename: 'bundle.js',
     },
     module: {
         loaders: [
-           {
-            test: /\.js/,     
-            loader: 'babel-loader',
-            include: __dirname + '/src',
-           },
+            {
+                test: /\.js/,     
+                loader: 'babel-loader',
+                include: __dirname + '/src',
+            },
+            {
+                test: /\.css/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html/,
+                loader: 'html-loader',
+            },
         ],
     }
 }
